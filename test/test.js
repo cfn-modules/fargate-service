627cec7fc322581d562ccc652b131b234cdbf16a
Michael Wittig <post@michaelwittig.info>
2019-01-03 18:15:10 +0100

    init (#1)

---
+++ test/test.js
@@ -0,0 +1,44 @@
+const test = require('ava');
+const cfntest = require('@cfn-modules/test');
+const axios = require('axios');
+
+test('defaults', async t => {
+  const stackName = cfntest.stackName();
+  try {
+    t.log(await cfntest.createStack(`${__dirname}/defaults.yml`, stackName, {}));
+    // what could we test here?
+  } finally {
+    t.log(await cfntest.deleteStack(stackName));
+    t.pass();
+  }
+});
+
+test('alb-single', async t => {
+  const stackName = cfntest.stackName();
+  try {
+    t.log(await cfntest.createStack(`${__dirname}/alb-single.yml`, stackName, {}));
+    const outputs = await cfntest.getStackOutputs(stackName);
+    t.log(outputs);
+    const res = await axios.get(`http://${outputs.DnsName}`);
+    t.is(res.headers.server, 'Apache/2.4.23 (Unix)');
+    t.true(res.data.includes('It works!'));
+  } finally {
+    t.log(await cfntest.deleteStack(stackName));
+    t.pass();
+  }
+});
+
+test('nlb-single', async t => {
+  const stackName = cfntest.stackName();
+  try {
+    t.log(await cfntest.createStack(`${__dirname}/nlb-single.yml`, stackName, {}));
+    const outputs = await cfntest.getStackOutputs(stackName);
+    t.log(outputs);
+    const res = await axios.get(`http://${outputs.DnsName}`);
+    t.is(res.headers.server, 'Apache/2.4.23 (Unix)');
+    t.true(res.data.includes('It works!'));
+  } finally {
+    t.log(await cfntest.deleteStack(stackName));
+    t.pass();
+  }
+});
