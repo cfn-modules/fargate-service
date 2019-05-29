fa62329a351e118cb6c1a2bb480a74d8210baab2
Michael Wittig <michael@widdix.de>
2019-05-29 19:50:02 +0200

    make cluster optional

--- test/test.js
+++ test/test.js
@@ -13,6 +13,17 @@ test.serial('defaults', async t => {
   }
 });
 
+test.serial('cluster', async t => {
+  const stackName = cfntest.stackName();
+  try {
+    t.log(await cfntest.createStack(`${__dirname}/cluster.yml`, stackName, {}));
+    // what could we test here?
+  } finally {
+    t.log(await cfntest.deleteStack(stackName));
+    t.pass();
+  }
+});
+
 test.serial('alb-single', async t => {
   const stackName = cfntest.stackName();
   try {
