75d2e8ce891a80f79e4608a60f2774cfa533c860
Michael Wittig <michael@widdix.de>
2019-07-08 07:37:24 +0200

    native secret module support for environment (#10)

---
+++ test/alb.js
@@ -0,0 +1,18 @@
+const test = require('ava');
+const cfntest = require('@cfn-modules/test');
+const axios = require('axios');
+
+test.serial('alb-single', async t => {
+  const stackName = cfntest.stackName();
+  try {
+    t.log(await cfntest.createStack(`${__dirname}/alb-single.yml`, stackName, {}));
+    const outputs = await cfntest.getStackOutputs(stackName);
+    t.log(outputs);
+    const res = await axios.get(`http://${outputs.DnsName}`);
+    t.is(res.headers.server, 'Apache/2.4.23 (Unix)');
+    t.true(res.data.includes('It works!'));
+  } finally {
+    t.log(await cfntest.deleteStack(stackName));
+    t.pass();
+  }
+});
