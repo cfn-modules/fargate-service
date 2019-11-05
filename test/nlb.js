acb18f3dc120599db9b83a79bf10e42c9e138739
Michael Wittig <michael@widdix.de>
2019-11-05 11:30:21 +0100

    update test dependencies

--- test/nlb.js
+++ test/nlb.js
@@ -1,6 +1,5 @@
 const test = require('ava');
 const cfntest = require('@cfn-modules/test');
-const axios = require('axios');
 
 test.serial('nlb-single', async t => {
   const stackName = cfntest.stackName();
@@ -8,7 +7,7 @@ test.serial('nlb-single', async t => {
     t.log(await cfntest.createStack(`${__dirname}/nlb-single.yml`, stackName, {}));
     const outputs = await cfntest.getStackOutputs(stackName);
     t.log(outputs);
-    const res = await axios.get(`http://${outputs.DnsName}`);
+    const res = await cfntest.probeHttpGet(`http://${outputs.DnsName}`);
     t.is(res.headers.server, 'Apache/2.4.23 (Unix)');
     t.true(res.data.includes('It works!'));
   } finally {
