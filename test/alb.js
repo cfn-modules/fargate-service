118bbaea0162bc08add294535e865b279e4bf49e
Michael Wittig <michael@widdix.de>
2019-11-05 11:54:10 +0100

    update test dependencies (#19)

--- test/alb.js
+++ test/alb.js
@@ -1,6 +1,5 @@
 const test = require('ava');
 const cfntest = require('@cfn-modules/test');
-const axios = require('axios');
 
 test.serial('alb-single', async t => {
   const stackName = cfntest.stackName();
@@ -8,7 +7,7 @@ test.serial('alb-single', async t => {
     t.log(await cfntest.createStack(`${__dirname}/alb-single.yml`, stackName, {}));
     const outputs = await cfntest.getStackOutputs(stackName);
     t.log(outputs);
-    const res = await axios.get(`http://${outputs.DnsName}`);
+    const res = await cfntest.probeHttpGet(`http://${outputs.DnsName}`);
     t.is(res.headers.server, 'Apache/2.4.23 (Unix)');
     t.true(res.data.includes('It works!'));
   } finally {
