e3182363237fb763a1166aabae6d7a388dd6ab8b
Andreas Wittig <andreas@widdix.de>
2022-05-05 14:03:40 +0200

    Improved ARM test

--- test/arm.js
+++ test/arm.js
@@ -5,7 +5,11 @@ test.serial('arm', async t => {
   const stackName = cfntest.stackName();
   try {
     t.log(await cfntest.createStack(`${__dirname}/arm.yml`, stackName, {}));
-    // what could we test here?
+    const outputs = await cfntest.getStackOutputs(stackName);
+    t.log(outputs);
+    const res = await cfntest.probeHttpGet(`http://${outputs.DnsName}`);
+    t.is(res.headers.server, 'nginx/1.20.2');
+    t.true(res.data.includes('Hello World! (v1)'));
   } finally {
     t.log(await cfntest.deleteStack(stackName));
     t.pass();
